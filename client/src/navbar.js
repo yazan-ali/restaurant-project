1064018fe18c5154a22de29f88123e9288df94ce
yazan ali <yazanabuali2000@gmail.com>
2020-09-05 13:46:27 +0300

    edit somethings 22

--- client/src/navbar.js
+++ client/src/navbar.js
@@ -69,7 +69,7 @@ class Navbar extends Component{
       </li>
       {/* {this.props.userId && */}
       <div>
-      <li className="nav-item">
+      <li className="nav-item justify-content-end">
         <a className="nav-link main-nav-link" href={`/cart/user/${this.props.userId}`}> <ShoppingCartIcon /> </a>
       </li>
       </div>
